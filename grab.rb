30257385dd8189678b5ecffd021137f43c1c94cd
Giles Bowkett <gilesb@gmail.com>
2011-06-17 21:35:40 -0700

    mostly works, altho completely untested
    * recursive method to get each as-yet-unseen link
    * sorting method for Link model (to order results)
    * page_number method for Link Model (for ordering)
    * everything into methods
    * some hacky "test" stuff at bottom

--- grab.rb
+++ grab.rb
@@ -8,27 +8,30 @@ ActiveRecord::Base.establish_connection(:adapter  => "sqlite3",
                                         :database => "links.sqlite3")
 
 class Link < ActiveRecord::Base
-end
-
-agent = Mechanize.new
-agent.get("http://mspaintadventures.com") do |page|
-  first_link_in_list = page.links[21]
-
-  # if this is new, continue through page.links until the link already seen, and show every
-  # link in that subset, excluding of course the one already seen. otherwise, simply wait until
-  # cron runs again. also update the DB with the latest value for most recent link.
-
-  known_link = Link.where(:href => first_link_in_list.href).first
-  unless known_link
-    Link.create!(:href => first_link_in_list.href,
-                 :title => first_link_in_list.title)
-    # continue for all the links in the list, until you find one that's been seen (this bit is
-    # probably recursive) and then open those links in a browser, or send me an e-mail, or
-    # ping me on Twitter, or something.
-
-    # Link.delete_all (but latest) (or something) -- it'd be cool to here to just throw out all
-    # but the most current link, since the only reason to retain that information is to know if
-    # links on the site are new or not.
+  def page_number
+    self.href.match(/p=([0-9]+)/)[1]
+  end
+  def self.sorted
+    all.sort_by &:page_number
   end
 end
+
+def gather_new_links(links)
+  link = links.shift
+  unless Link.where(:href => link.href).first
+    Link.create!(:href => link.href, :text => link.text)
+    gather_new_links(links)
+  end
+end
+
+def check_web_page
+  agent = Mechanize.new
+  agent.get("http://mspaintadventures.com") do |page|
+    # magic numbers! links[21..65] represent all the "Latest Pages" links in the MSPA UI
+    gather_new_links(page.links[21..65])
+  end
+end
+
+check_web_page
+Link.sorted.reverse.each {|l| puts l.page_number}
 
